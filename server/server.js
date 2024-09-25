d48f000f306c7b06f4408af649162f58354ffd4e
FBetulDemir <fatmabetuld@gmail.com>
2024-09-25 12:30:57 +0200

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const corseOptions = {
+  origin: ["http://localhost:5173"],
+};
+
+app.use(cors(corseOptions));
+
+app.get("/api", (req, res) => {
+  res.json({ fruits: ["banana", "apple", "cherry", "strawberry"] });
+});
+
+app.listen(8080, () => {
+  console.log("Server is running on port 8080");
+});
